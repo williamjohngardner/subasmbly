1c9fab899c677f04a466ac88d42b45ac1a7b9bae
williamjohngardner <bill@williamjohngardner.com>
2020-01-20 17:29:17 -0500

    Procfile

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser()); // parses cookie info from incoming request
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ROUTES
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
