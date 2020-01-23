0da7014289effa50f750fccce17bf34b8225e052
williamjohngardner <bill@williamjohngardner.com>
2020-01-23 18:50:48 -0500

    jade templates scaffolded

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,17 @@
 /* GET home page */
-module.exports.index = function(req, res) {
-    res.render('index', { title: 'Express' });
+module.exports.dashboard = function(req, res) {
+    // index is the Jade view template to display
+    res.render('index', { title: 'Dashboard' });
+};
+
+module.exports.projects = function(req, res) {
+    res.render('index', { title: 'Projects' });
+};
+
+module.exports.subassemblies = function(req, res) {
+    res.render('index', { title: 'Subassembly' });
+};
+
+module.exports.parts = function(req, res) {
+    res.render('parts', { title: 'Parts Listing' });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 const ctrlMain = require('../controllers/main');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* GET dashboard page. */
+router.get('/', ctrlMain.dashboard);
+
+/* GET projects page. */
+router.get('/projects', ctrlMain.projects);
+
+/* GET subassemblies page. */
+router.get('/subassemblies', ctrlMain.subassemblies);
+
+/* GET parts page. */
+router.get('/parts', ctrlMain.parts);
 
 module.exports = router;
