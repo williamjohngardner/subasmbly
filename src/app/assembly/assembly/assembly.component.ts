67d8bcb946cac50e9ba167238ec6cc54ac5301f9
williamjohngardner <bill@williamjohngardner.com>
2020-07-21 17:57:45 -0400

    part sub assy project low level UI all standardized

--- src/app/assembly/assembly/assembly.component.ts
+++ src/app/assembly/assembly/assembly.component.ts
@@ -34,18 +34,42 @@ export class AssemblyComponent implements OnInit {
       description: [{ value: this._assembly['description'], disabled: false }],
       category: [{ value: this._assembly['category'], disabled: false }],
       subCategory: [{ value: this._assembly['Category'], disabled: false }],
-      parts: this._formBuilder.group({
-        partName: [{ value: this._assembly['parts'], disabled: false }]
-      }),
-      subassemblies: this._formBuilder.group({
-        subassemblyName: [{ value: this._assembly['subassemblies'], disabled: false }]
-      }),
+      parts: this._formBuilder.array([]),
+      subassemblies: this._formBuilder.array([]),
       uom: [{ value: this._assembly['uom'], disabled: false }],
       unitCost: [{ value: this._assembly['unitCost'], disabled: false }],
       unitPrice: [{ value: this._assembly['unitPrice'], disabled: false }],
       dateCreated: [{ value: this._assembly['_dateCreated'], disabled: false }],
       dateUpdated: [{ value: this._assembly['_dateUpdated'], disabled: false }]
     })
+  }
+
+  createPartsArray () {
+    const control: FormArray = this.assemblyForm.get('parts') as FormArray;
+    for (let item = 0; item < this._assembly['parts'].length; item++) {
+      control.push(this.createPartsFormGroup(this._assembly['parts'][item]));
+    }
+  }
+
+  createSubassemblyArray () {
+    const control: FormArray = this.assemblyForm.get('subassemblies') as FormArray;
+    for (let item = 0; item < this._assembly['subassemblies'].length; item++) {
+      control.push(this.createSubassemblyFormGroup(this._assembly['subassemblies'][item]));
+    }
+  }
+
+  createPartsFormGroup (part: any) {
+    return new FormGroup({
+      partName: new FormControl(part.partName),
+      _id: new FormControl(part._id),
+    });
+  }
+
+  createSubassemblyFormGroup (subassembly: any) {
+    return new FormGroup({
+      subassemblyName: new FormControl(subassembly.subassemblyName),
+      _id: new FormControl(subassembly._id),
+    });
   }
 
   updateAssembly() {
--- src/app/project/project/project.component.ts
+++ src/app/project/project/project.component.ts
@@ -34,20 +34,56 @@ export class ProjectComponent implements OnInit {
       description: [{ value: this._project['description'], disabled: false }],
       category: [{ value: this._project['category'], disabled: false }],
       subCategory: [{ value: this._project['subCategory'], disabled: false }],
-      parts: this._formBuilder.group({
-        partName: [{ value: this._project['parts']['partName'], disabled: false }]
-      }),
-      subassemblies: this._formBuilder.group({
-        subassemblyName: [{ value: this._project['subassemblies']['subassemblyName'], disabled: false }]
-      }),
-      assemblies: this._formBuilder.group({
-        assemblyName: [{ value: this._project['assemblies']['assemblyName'], disabled: false }]
-      }),
+      parts: this._formBuilder.array([]),
+      subassemblies: this._formBuilder.array([]),
+      assemblies: this._formBuilder.array([]),
       projectCost: [{ value: this._project['projectCost'], disabled: false }],
       projectPrice: [{ value: this._project['projectPrice'], disabled: false }],
       dateCreated: [{ value: this._project['_dateCreated'], disabled: false }],
       dateUpdated: [{ value: this._project['_dateUpdated'], disabled: false }]
     })
+  }
+
+    createPartsArray () {
+    const control: FormArray = this.projectForm.get('parts') as FormArray;
+    for (let item = 0; item < this._project['parts'].length; item++) {
+      control.push(this.createPartsFormGroup(this._project['parts'][item]));
+    }
+  }
+
+  createSubassemblyArray () {
+    const control: FormArray = this.projectForm.get('subassemblies') as FormArray;
+    for (let item = 0; item < this._project['subassemblies'].length; item++) {
+      control.push(this.createSubassemblyFormGroup(this._project['subassemblies'][item]));
+    }
+  }
+
+  createAssemblyArray () {
+    const control: FormArray = this.projectForm.get('assemblies') as FormArray;
+    for (let item = 0; item < this._project['assemblies'].length; item++) {
+      control.push(this.createAssemblyFormGroup(this._project['assemblies'][item]));
+    }
+  }
+
+  createPartsFormGroup (part: any) {
+    return new FormGroup({
+      partName: new FormControl(part.partName),
+      _id: new FormControl(part._id),
+    });
+  }
+
+  createSubassemblyFormGroup (subassembly: any) {
+    return new FormGroup({
+      subassemblyName: new FormControl(subassembly.subassemblyName),
+      _id: new FormControl(subassembly._id),
+    });
+  }
+
+  createAssemblyFormGroup (assembly: any) {
+    return new FormGroup({
+      assemblyName: new FormControl(assembly.assemblyName),
+      _id: new FormControl(assembly._id),
+    });
   }
 
   updateProject() {
