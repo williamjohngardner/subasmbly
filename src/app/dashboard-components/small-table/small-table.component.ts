438232f5bc94961aafc0ea0a818289957fd8eaf3
Bill Gardner <billgardner@localhost.localdomain>
2020-11-19 17:52:53 -0500

    resolved content projection errors

--- src/app/dashboard-components/small-table/small-table.component.ts
+++ src/app/dashboard-components/small-table/small-table.component.ts
@@ -6,15 +6,34 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./small-table.component.css']
 })
 export class SmallTableComponent implements OnInit {
-  @Input() compHeight: string;
-  
+  @Input() componentHeight: string;
+  @Input() numberOfRows: number;
+
   public data: Array<object> = [
-    { header1: 'Assembly 1',
+    {
+      header1: 'Assembly 1',
       header2: 345
     },
-    { header1: 'Assembly 2',
+    {
+      header1: 'Assembly 2',
       header2: 7688
-  }
+    },
+    {
+      header1: 'Assembly 3',
+      header2: 7598
+    },
+    {
+      header1: 'Assembly 4',
+      header2: 121
+    },
+    {
+      header1: 'Assembly 5',
+      header2: 34521
+    },
+    {
+      header1: 'Assembly 6',
+      header2: 25436
+    }
   ]
 
   constructor() { }
--- src/app/inventory/assembly/assembly-dashboard/assembly-dashboard.component.ts
+++ src/app/inventory/assembly/assembly-dashboard/assembly-dashboard.component.ts
@@ -11,6 +11,8 @@ import { Observable } from 'rxjs'
 })
 export class AssemblyDashboardComponent {
   public _assembly: Observable<object> = this._route.data;
+  public threeSixtyFive: string = '365px';
+  public twoNinetyNine: string = '299px';
 
   constructor(
     readonly http: HttpClient,
